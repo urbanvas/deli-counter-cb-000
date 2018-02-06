dd1b5076eeeb872f5638206894e945b997fc1db7
Neil Seejoor <nvseejoor@gmail.com>
2018-02-06 01:32:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,8 +10,7 @@ def line(array)
   end
 end
 
-def take_a_number(array, name)
-
+def take_a_number(katz_deli, name)
     katz_deli << name
-    puts "Welcome, #{name}. You are number #{array.size + 1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
